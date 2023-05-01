875a5f4757886f7d269e5c6aa88fe2f18061f836
YentlHendrickx <yentlhendrickx@outlook.com>
2023-05-01 15:16:58 +0200

    init

---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+// Import images
+import WebImage1 from '../images/web-project-1.jpg';
+import WebImage2 from '../images/web-project-2.jpg';
+import MobileImage1 from '../images/mobile-project-1.jpg';
+import MobileImage2 from '../images/mobile-project-2.jpg';
+import UIImage1 from '../images/ui-project-1.jpg';
+import UIImage2 from '../images/ui-project-2.jpg';
+
+import MemoryLog from '../images/memorylogMain.png';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Memorylog Laravel',
+		category: 'Web Application',
+		img: MemoryLog,
+		ProjectHeader: {
+			title: 'Application for patients with Alzheimers',
+			publishDate: 'May 1, 2023',
+			tags: 'Full-Stack',
+		},
+	},
+];
